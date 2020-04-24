01fd31161a4ec0239cdef6707d40bb39a3d5ad3b
Onur Kaplan <onur.kaplan@mynetgroup.com>
2020-04-24 04:00:17 +0300

    add timer function in the project

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ let quizItemTemplate = "";
 let quizItemContainer =  document.querySelector('.quiz-item-container');
 let quizChoicesContainer =  document.querySelector('.answer-choices-box');
 let selectedAnswerResult =  document.querySelector('.selected-answer-result');
+let questionTitle =  document.querySelector('.question-title');
+let startQuiz =  document.querySelector('.start-to-quiz');
+let userPointBox =  document.querySelector('.user-point');
 let resultModal =  document.querySelector('#resultModal');
 let showResult =  document.querySelector('#show-result');
+
+
 
 let  allQuestionsData = [
   {
@@ -71,36 +76,52 @@ let correctAnswers = [
 
 let selectedData = [];
 
+let unitePoint = 100 / allQuestionsData.length ;
 
-allQuestionsData.map(questions => {
-  quizItemTemplate += `<div class="card answer-item-box">
-  <button class="btn btn-dark btn-lg btn-block mb-1 rounded-0 text-left" type="button" data-toggle="collapse" data-target="#q-${questions.questionID}">
-    <span class="badge badge-light mr-2">${questions.questionID} </span> ${questions.question}
-  </button>
-  <div id="q-${questions.questionID}" class="collapse" data-parent="#accordionExample">
-    <div class="card-body answer-choices-box">
-        ${
-          questions.answers.map(items => 
-            `
-            <div class="d-flex align-items-center mb-3">
-              <input type="radio" id="op-${questions.questionID}-${items.opC}" name="customRadio" class="mr-3" 
-              onchange="setAnswer('${questions.questionID}', '${items.opC}', '${items.opT}')">
-              <label class="mb-0 w-100 alert alert-secondary" for="op-${questions.questionID}-${items.opC}">
-              <span class="badge badge-light mr-2">${items.opC} </span>
-              ${items.opT}</label>
-            </div>
-            `
-          ).join("")
-        }
+
+
+ 
+function renderToData() {
+  allQuestionsData.map(questions => {
+    quizItemTemplate += `<div class="card answer-item-box">
+    <button class="question-item-btn btn btn-dark btn-lg btn-block mb-1 rounded-0 text-left" type="button" data-toggle="collapse" data-target="#q-${questions.questionID}">
+      <span class="badge badge-light mr-2">${questions.questionID} </span> ${questions.question}
+    </button>
+    <div id="q-${questions.questionID}" class="collapse" data-parent="#accordionExample">
+      <div class="card-body answer-choices-box">
+          ${
+            questions.answers.map(items => 
+              `
+              <div class="d-flex align-items-center mb-3">
+                <input type="radio" id="op-${questions.questionID}-${items.opC}" name="customRadio" class="mr-3" 
+                onchange="setAnswer('${questions.questionID}', '${items.opC}', '${items.opT}')">
+                <label class="mb-0 w-100 alert alert-secondary" for="op-${questions.questionID}-${items.opC}">
+                <span class="badge badge-light mr-2">${items.opC} </span>
+                ${items.opT}</label>
+              </div>
+              `
+            ).join("")
+          }
+      </div>
     </div>
-  </div>
-  </div>`
-});
+    </div>`
+  });
+  quizItemContainer.innerHTML = quizItemTemplate;
+}
 
-quizItemContainer.innerHTML = quizItemTemplate;
+
+
+
+
+startQuiz.addEventListener("click", function(){
+  this.classList.toggle("d-none");
+  questionTitle.style.display = "block";
+  renderToData();
+  countdown(1, 30);
+})
 
 function setAnswer(id, sign, name) {
-  let newObj = { answerID: parseInt(id), answerSign:  sign, answerName: name};
+  let newObj = { answerID: parseInt(id), answerSign:  sign, answerName: name, correctSign: false};
   if (selectedData.filter(item => item.answerID === parseInt(id)).length == 0) {
     selectedData.push(newObj);
   }
@@ -108,43 +129,82 @@ function setAnswer(id, sign, name) {
     let answerValues = selectedData.filter(item => item.answerID === newObj.answerID)[0];
     answerValues.answerSign = sign;
     answerValues.answerName = name;
+    answerValues.correctSign = false;
   }
 
 
 correctAnswers.filter(item => item.questionID === parseInt(id))
 .filter(fin => {
   if(fin.correctAnswer === sign){
-    console.log("aynıddd")
+    selectedData.filter(item => item.answerID === newObj.answerID)[0].correctSign = true;
   }
 }) 
 
-  // let arr3 = correctAnswers.map((item, i) => Object.assign({}, item, selectedData[i]));
-
-console.log(selectedData);
+// All questions are solved controler
+selectedData.length === allQuestionsData.length ? showResult.style.display = "block" : "none";
 
 }
 
-
+// Show ressult
 showResult.addEventListener("click", function() {
-
- 
   let selectedAnswersTemplate = "";
-
   selectedData.sort(function (a, b) {
     return a.answerID - b.answerID;
   });
 
   selectedData.map(item => {
     selectedAnswersTemplate += `
-        <div class="alert alert-success">
-          <span class="badge badge-light mr-2">${item.answerID}. Soru - ${item.answerSign.toUpperCase()} </span> ${item.answerName}
+        <div class="alert alert-${item.correctSign == true ? "success" : "danger"}">
+          <span class="badge badge-light mr-2">${item.answerID}. Soru - ${item.answerSign.toUpperCase()} </span> 
+          ${item.correctSign == true ? "<span class='badge badge-success'>Doğru</span>" : "<span class='badge badge-danger'>Yanlış</span>"}
+          ${item.answerName}
         </div>
     `;
     })
     selectedAnswerResult.innerHTML = selectedAnswersTemplate;
 
+    let totapPoint = selectedData.filter(trueVal => trueVal.correctSign == true).reduce((calc) => {
+        return calc + unitePoint
+    }, 0);
 
+    userPointBox.innerHTML = totapPoint;
+    
+    setQuestionsDisabled()
 })
+
+
+// Timer
+function countdown(min, sec) {
+  let second = sec;
+  let minute = min;
+  function tick() {
+      var counter = document.querySelector("#count-down");
+      second--;
+      let currentTime =  minute-1 + ":" + (second < 10 ? "0" : "") + second;
+      counter.innerHTML = currentTime;
+      if( second > 0 ) {
+          setTimeout(tick, 1000);
+      } else {
+          if(minute > 1){
+              countdown(minute-1, 60);           
+          } else{
+            setQuestionsDisabled()
+          }
+          
+      }
+  }
+  tick();
+}
+
+// If the time is over all question items are disabled
+function setQuestionsDisabled() {
+  let questionItemBtn =  document.querySelectorAll('.question-item-btn');
+  questionItemBtn.forEach(function(item) {
+    item.setAttribute("disabled", "true");
+  });
+}
+
+ 
 
 
 
